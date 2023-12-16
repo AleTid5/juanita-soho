d7913499901e9cf9b2a2170f4a793bc693cbae29
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-12-16 01:42:04 +0100

    Use Base Path

--- utils/imagesUtils.ts
+++ utils/imagesUtils.ts
@@ -1,4 +1,3 @@
-const BASE_URL =
-  'https://firebasestorage.googleapis.com/v0/b/juanita-soho.appspot.com/o/';
+const BASE_URL = '/juanita-soho/assets/images';
 
-export const getImage = (image: string) => `${BASE_URL}${image}?alt=media`;
+export const getImage = (image: string) => `${BASE_URL}/${image}`;

41d22d440e9ded0dadb60a3c4546045d9027d90a
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-12-16 12:03:03 +0100

    Change icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,8 +10,12 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="shortcut icon" href="juanita-soho/favicon.ico" type="image/x-icon" />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/juanita-soho/apple-touch-icon.png"
+          />
           <meta name="theme-color" content="#ffffff" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />

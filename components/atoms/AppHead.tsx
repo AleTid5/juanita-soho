1b2ee13bb29e259138951370a409a37506becc37
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-12-22 13:56:58 +0100

    Add logo banner

--- components/atoms/AppHead.tsx
+++ components/atoms/AppHead.tsx
@@ -1,13 +1,9 @@
 import Head from 'next/head';
 
-const mainText = 'Juanita Soho: Vacation Rentals & Unique Homes';
+const title = 'Juanita Soho: Vacation Rentals & Unique Homes';
+const description = 'The greatest outdoors, in the vibrant neighborhood of Palermo.';
 
-type AppHeadProps = {
-  title?: string;
-  description?: string;
-};
-
-const AppHead = ({ title = mainText, description = mainText }: AppHeadProps) => {
+const AppHead = () => {
   return (
     <Head>
       <title>{title}</title>
@@ -15,6 +11,12 @@ const AppHead = ({ title = mainText, description = mainText }: AppHeadProps) => 
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="628" />
+      <meta property="og:image" content="/assets/images/logoBanner.png" />
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content={description} />
+      <meta name="twitter:image" content="/assets/images/logoBanner.png" />
     </Head>
   );
 };

edd3c453d73518c8ee60c197c8f2ca9997f23fb1
alejandro.tidele <alejandro.tidele@glovoapp.com>
2023-12-22 23:40:03 +0100

    Add Google Maps links

--- components/atoms/AppFooter.tsx
+++ components/atoms/AppFooter.tsx
@@ -1,50 +1,17 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-import { SITE_MAP } from 'constants/site-map';
 
 const AppFooter = () => {
+  const year = new Date().getFullYear();
+
   return (
     <footer className="bg-gray-100 border-t border-gray-200">
       <div className="container">
-        <div className="grid grid-cols-1 lg:grid-cols-4">
-          {SITE_MAP.map(({ title, sitemap }, index) => (
-            <div
-              key={title}
-              className={`${
-                index !== 0 && 'border-t border-gray-200 lg:border-none'
-              } py-6 md:py-8`}
-            >
-              <span className="inline-block mb-4 text-sm font-medium">{title}</span>
-              <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-1 gap-y-3 xl:gap-y-4">
-                {sitemap.map((data) => (
-                  <li
-                    key={data}
-                    className="text-sm text-gray-500 hover:text-gray-400 hover:underline"
-                  >
-                    <Link href="/">{data}</Link>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
         <div className="flex flex-col items-center justify-between py-5 text-sm text-gray-400 border-t border-gray-200 lg:py-6 lg:flex-row">
           <div className="flex flex-col items-center order-last lg:flex-row lg:order-none">
-            <span className="mr-0 text-center lg:mr-4">© 2021 Airbnb, Inc.</span>
-            <span className="mb-2 mr-0 lg:mr-8 lg:mb-0">
-              | Clone by{' '}
-              <a
-                href="https://edwintantawi.vercel.app"
-                target="_blank"
-                rel="noreferrer"
-                className="font-medium text-primary hover:underline"
-              >
-                Edwin Tantawi
-              </a>{' '}
-              |
-            </span>
-            <ul className="flex space-x-6 list-disc">
+            <span className="mr-0 text-center lg:mr-4">© {year} Juanita Soho</span>
+            <ul className="flex space-x-6 list-disc lg:ml-2">
               <li className="hover:underline">
                 <Link href="/">Privacy</Link>
               </li>
